601055c62603725a76a69767ef219df6907435a3
kayatsa <kayatsa8@gmail.com>
2024-10-30 13:45:55 +0200

    Backend: Tiles: HeroicUnit: changed cast_ability's parameter from Board to List[unit].

--- Backend/Tiles/HeroicUnit.py
+++ Backend/Tiles/HeroicUnit.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 from Backend.Board import Board
+from Backend.Tiles.Unit import Unit
 
 
 class HeroicUnit(ABC):
 
     @abstractmethod
-    def castAbility(self, board: Board) -> None:
+    def cast_ability(self, units: List[Unit]) -> None:
         pass
 
